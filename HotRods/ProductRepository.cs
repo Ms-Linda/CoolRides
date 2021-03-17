2b30f78ba6bf11d7f7c1d456706ae0a0196f7210
Ms-Linda <wfhlinda@gmail.com>
2021-03-17 13:43:27 -0400

    did steps to 17; started 18-add Index View file

---
+++ HotRods/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using HotRods.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotRods
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+
+    }
+}
