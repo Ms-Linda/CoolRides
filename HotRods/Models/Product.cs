7a3389f49746ef3d4efc881b0ea301f0654b9156
Ms-Linda <wfhlinda@gmail.com>
2021-03-17 01:32:28 -0400

    did to step 10; need to correct auto to product

---
+++ HotRods/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotRods.Models
+{
+    public class Auto
+    {
+        public int AutoID {get; set;}
+        public string Make {get; set;}
+        public int Model {get; set;}
+        public int ReviewID {get; set;}
+        public int Year {get; set;}
+        public int SpdN60 {get; set;}
+        public int Price {get; set;}
+
+
+
+
+}
+}
