0250ad34e3677bc903ee476524ad3d5098722184
Ms-Linda <wfhlinda@gmail.com>
2021-03-18 02:44:16 -0400

    corrected conn err-Model err from int to string

--- CoolRides/Models/Product.cs
+++ CoolRides/Models/Product.cs
@@ -13,7 +13,7 @@ namespace CoolRides.Models
 
         public int ProductID {get; set;}
         public string Make {get; set;}
-        public int Model {get; set;}
+        public string Model {get; set;}
         public int ReviewID {get; set;}
         public int Year {get; set;}
         public int In60 {get; set;}
